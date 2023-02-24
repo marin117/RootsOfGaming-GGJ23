7569ed92fa63c23050c6604594462869cf25ba40
Marin Jurjevic <croatiathebest@gmail.com>
2023-02-24 22:26:46 +0100

    Rename and refactor of level/world screen

---
+++ Pacman3D/world.gd
@@ -0,0 +1,29 @@
+extends Spatial
+
+export var num_ghosts = 1
+
+var is_chasing = false
+
+export(PackedScene) var ghost_scene = preload("res://Ghost/Ghost.tscn")
+var ghosts = []
+
+func _ready():
+	randomize()
+	spawn_ghost()
+
+func _process(_delta):
+	if Input.is_action_just_released("camera"):
+		$TopDownCamera.current = !$TopDownCamera.current
+		$Player.switch_movement(!$TopDownCamera.current)
+		$Level.toggle_fog(!$TopDownCamera.current)
+		#$DirectionalLight.visible = $TopDownCamera.current
+ 
+func spawn_ghost():
+	var ghost = ghost_scene.instance()
+	ghost.translate($Level.ghost_spawn_location)
+	ghost.scale_object_local(Vector3(1.7,1.7,1.7))
+	ghosts.append(ghost)
+	add_child(ghost)
+
+func _on_Player_player_dead():
+	get_tree().change_scene("res://Lose/LoseScreen.tscn")
