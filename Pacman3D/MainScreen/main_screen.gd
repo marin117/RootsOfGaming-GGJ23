f9bd809e4a2aa3e7100f31b0b23bf64994d6edab
Marin Jurjevic <croatiathebest@gmail.com>
2023-03-01 22:54:59 +0100

    minor refactors

--- Pacman3D/MainScreen/main_screen.gd
+++ Pacman3D/MainScreen/main_screen.gd
@@ -1,5 +1,4 @@
 extends Control
 
-
 func _on_PlayButton_pressed():
 	get_tree().change_scene("res://World.tscn")
--- Pacman3D/Maze/maze_lvl1.gd
+++
@@ -1,7 +0,0 @@
-extends BaseMaze
-
-func _ready():
-	ghost_spawn_location = $GhostSpawn.transform.origin
-	coin_path = get_node("CoinSpawn/CoinSpawnLocation")
-	environment = $WorldEnvironment.get_environment()
-	generate_coins()
